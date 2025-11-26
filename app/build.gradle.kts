c3751231547f3fcb009144280bd705570824e31a
THToufique <thtoufique@gmail.com>
2025-11-27 01:42:58 +0600

    Add Google apksig library for APK signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     // Icons
 
     // BouncyCastle for keystore
+
+    // Google apksig library
+    implementation("com.android.tools.build:apksig:8.7.2")
     implementation("org.bouncycastle:bcprov-jdk18on:1.78")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.78")
     implementation("androidx.compose.material:material-icons-extended:1.7.0")
