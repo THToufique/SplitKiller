9c2f2dff42c90a48ea859bc107e4fd248facf5f1
THToufique <thtoufique@gmail.com>
2025-11-27 01:42:27 +0600

    Implement Material You theme with amber/gunmetal color scheme

--- app/src/main/java/com/ripp3r/splitkiller/ui/theme/Color.kt
+++ app/src/main/java/com/ripp3r/splitkiller/ui/theme/Color.kt
@@ -2,10 +2,71 @@ package com.ripp3r.splitkiller.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val GunmetalDark = Color(0xFF1E1E1E)
+val GunmetalLight = Color(0xFF2D2D2D)
+val AmberPrimary = Color(0xFFFFB300)
+val AmberSecondary = Color(0xFFFFCA28)
+val OrangeAccent = Color(0xFFFF6D00)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val TextPrimary = Color(0xFFEEEEEE)
+val TextSecondary = Color(0xFFB0B0B0)
+
+// Dark Theme Colors
+val md_theme_dark_primary = AmberPrimary
+val md_theme_dark_onPrimary = Color(0xFF452B00)
+val md_theme_dark_primaryContainer = Color(0xFF633F00)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDF9E)
+val md_theme_dark_secondary = AmberSecondary
+val md_theme_dark_onSecondary = Color(0xFF412D00)
+val md_theme_dark_secondaryContainer = Color(0xFF5D4200)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFDEA5)
+val md_theme_dark_tertiary = OrangeAccent
+val md_theme_dark_onTertiary = Color(0xFF532200)
+val md_theme_dark_tertiaryContainer = Color(0xFF763300)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFDBC8)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = GunmetalDark
+val md_theme_dark_onBackground = TextPrimary
+val md_theme_dark_surface = GunmetalDark
+val md_theme_dark_onSurface = TextPrimary
+val md_theme_dark_surfaceVariant = GunmetalLight
+val md_theme_dark_onSurfaceVariant = TextSecondary
+val md_theme_dark_outline = Color(0xFF948F99)
+val md_theme_dark_inverseOnSurface = GunmetalDark
+val md_theme_dark_inverseSurface = TextPrimary
+val md_theme_dark_inversePrimary = Color(0xFF825500)
+val md_theme_dark_surfaceTint = AmberPrimary
+
+// Light Theme Colors
+val md_theme_light_primary = Color(0xFF825500)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFFFDF9E)
+val md_theme_light_onPrimaryContainer = Color(0xFF291800)
+val md_theme_light_secondary = Color(0xFF7A5900)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFFDEA5)
+val md_theme_light_onSecondaryContainer = Color(0xFF261900)
+val md_theme_light_tertiary = Color(0xFF994600)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFDBC8)
+val md_theme_light_onTertiaryContainer = Color(0xFF321200)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF1E1B16)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF1E1B16)
+val md_theme_light_surfaceVariant = Color(0xFFEBE1CF)
+val md_theme_light_onSurfaceVariant = Color(0xFF4C4639)
+val md_theme_light_outline = Color(0xFF7D7667)
+val md_theme_light_inverseOnSurface = Color(0xFFF7EFE7)
+val md_theme_light_inverseSurface = Color(0xFF33302A)
+val md_theme_light_inversePrimary = AmberPrimary
+val md_theme_light_surfaceTint = Color(0xFF825500)
